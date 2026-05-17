cfc4e188d6641eb0ee783cd1621ae642c9892183
Thayne McCombs <astrothayne@gmail.com>
2026-05-17 14:29:35 -0600

    refactor: Switch from nom to winnow
    Because winnow seems better maintained, and easier to use.

--- src/desktop/error.rs
+++ src/desktop/error.rs
@@ -1,14 +1,11 @@
 use std::error;
 use std::fmt;
 use std::io;
-use std::str::Utf8Error;
-
-use nom::error::ErrorKind;
 
 #[derive(Debug)]
 pub enum ParseError {
     /// Syntax error
-    Syntax(String, ErrorKind),
+    Syntax(String),
     /// Invalid UTF-8 sequence
     NonUtf8,
     /// IO error
@@ -20,7 +17,7 @@ impl std::cmp::PartialEq for ParseError {
     fn eq(&self, other: &Self) -> bool {
         use self::ParseError::*;
         match (self, other) {
-            (Syntax(ref s1, k1), Syntax(ref s2, k2)) => k1 == k2 && s1 == s2,
+            (Syntax(ref e1), Syntax(e2)) => e1 == e2,
             (NonUtf8, NonUtf8) => true,
             _ => false,
         }
@@ -31,7 +28,7 @@ impl fmt::Display for ParseError {
     fn fmt(&self, fmt: &mut fmt::Formatter) -> fmt::Result {
         use self::ParseError::*;
         match *self {
-            Syntax(ref i, kind) => write!(fmt, "Invalid syntax [{:?}] at \"{}\"", kind, i),
+            Syntax(ref e) => write!(fmt, "Invalid syntax: {}", e),
             NonUtf8 => write!(fmt, "Invalid Ut8 byte sequence in input"),
             IO(ref err) => write!(fmt, "{}", err),
         }
@@ -50,24 +47,5 @@ impl error::Error for ParseError {
 impl From<io::Error> for ParseError {
     fn from(err: io::Error) -> Self {
         ParseError::IO(err)
-    }
-}
-
-impl nom::error::ParseError<&[u8]> for ParseError {
-    fn from_error_kind(input: &[u8], kind: ErrorKind) -> Self {
-        let input_sample = input[..std::cmp::min(input.len(), 16)]
-            .escape_ascii()
-            .to_string();
-        ParseError::Syntax(input_sample, kind)
-    }
-
-    fn append(_i: &[u8], _kind: ErrorKind, other: Self) -> Self {
-        other
-    }
-}
-
-impl nom::error::FromExternalError<&[u8], Utf8Error> for ParseError {
-    fn from_external_error(_: &[u8], _: ErrorKind, _: Utf8Error) -> Self {
-        Self::NonUtf8
     }
 }
--- src/desktop/parser.rs
+++ src/desktop/parser.rs
@@ -3,108 +3,98 @@ use std::fs::File;
 use std::io;
 use std::path::Path;
 use std::str;
-use std::sync::OnceLock;
 
-use nom::{
-    bytes::complete::take_while,
-    character::complete::{char, space0},
-    combinator::{all_consuming, eof, map, map_res, value},
-    multi::{fold_many0, many0},
-    sequence::{delimited, preceded, separated_pair, terminated},
-    Finish, InputTakeAtPosition, Parser,
-};
-use nom_regex::bytes::re_find;
-use regex::bytes::Regex;
+use winnow::ascii::{space0, till_line_ending};
+use winnow::combinator::{alt, delimited, opt, preceded, repeat, separated_pair, terminated};
+use winnow::stream::{AsChar, Stream};
+use winnow::token::take_while;
+use winnow::{Parser, Result};
 
 use super::error::*;
 use super::model::*;
 
 pub type ParseResult = Result<DesktopEntry, ParseError>;
 
-type IResult<'a, T> = nom::IResult<&'a [u8], T, ParseError>;
-
 /// Parse a slice of bytes into a `DesktopEntry`.
 ///
 /// This parses a .desktop file (or similar) into a `DesktopEntry`.
 /// If it is unable to successfully parse it returns an `Err`
-pub fn parse<T: AsRef<[u8]>>(input: T) -> ParseResult {
-    all_consuming(desktop_entry)(input.as_ref())
-        .finish()
-        .map(|(_, e)| e)
+pub fn parse(input: &[u8]) -> ParseResult {
+    desktop_entry
+        .parse(input)
+        .map_err(|e| ParseError::Syntax(e.to_string()))
 }
 
 pub fn parse_io<T: io::Read>(input: &mut T) -> ParseResult {
     let mut buf = Vec::new();
     input.read_to_end(&mut buf)?;
-    parse(buf)
+    parse(buf.as_ref())
 }
 
 pub fn parse_file<T: AsRef<Path>>(path: T) -> ParseResult {
     parse_io(&mut File::open(path)?)
 }
 
-fn desktop_entry(input: &[u8]) -> IResult<'_, DesktopEntry> {
-    preceded(blanks, map(many0(group), DesktopEntry::new))(input)
+fn desktop_entry(input: &mut &[u8]) -> Result<DesktopEntry> {
+    preceded(blanks, repeat(0.., group).map(DesktopEntry::new)).parse_next(input)
 }
 
-fn group(i: &[u8]) -> IResult<'_, Group> {
-    let header = delimited(char('['), take_while(is_header_char), char(']'));
+fn group(i: &mut &[u8]) -> Result<Group> {
+    let mut header = delimited(b'[', take_while(0.., is_header_char), b']').try_map(str::from_utf8);
 
-    let (i, name) = map_res(header, str::from_utf8)(i)?;
-    let (i, values) = delimited(char('\n'), key_value_list, blanks)(i)?;
-    Ok((i, Group::new(name.into(), values)))
+    let name = header.parse_next(i)?;
+    let values = delimited(b'\n', key_value_list, blanks).parse_next(i)?;
+    Ok(Group::new(name.into(), values))
 }
 
 // If we ever support serialization, we need a way to preserve comments
-fn comment(i: &[u8]) -> IResult<'_, &[u8]> {
-    let endline = char('\n').or(value('\0', eof));
-    delimited(char('#'), take_while(|c| c != b'\n'), endline)(i)
+fn comment<'s>(i: &mut &'s [u8]) -> Result<&'s [u8]> {
+    preceded('#', till_line_ending).parse_next(i)
 }
-fn blanks(i: &[u8]) -> IResult<'_, ()> {
-    let empty_line = terminated(space0, char('\n'));
-    fold_many0(empty_line.or(comment), || (), |_, _| ())(i)
-}
-
-fn key_value_list(i: &[u8]) -> IResult<'_, HashMap<String, String>> {
-    fold_many0(entry, HashMap::new, |mut acc, item| {
-        acc.insert(item.0, item.1);
-        acc
-    })(i)
+fn blanks(i: &mut &[u8]) -> Result<()> {
+    let empty_line = terminated(space0, '\n');
+    repeat(0.., alt((empty_line, comment))).parse_next(i)
 }
 
-fn entry(i: &[u8]) -> IResult<'_, (String, String)> {
-    eprintln!("parsing entry: {}", str::from_utf8(i).unwrap_or(""));
+fn key_value_list(i: &mut &[u8]) -> Result<HashMap<String, String>> {
+    repeat(0.., entry).parse_next(i)
+}
+
+fn entry(i: &mut &[u8]) -> Result<(String, String)> {
     separated_pair(
         preceded(blanks, entry_key),
-        delimited(space0, char('='), space0),
+        delimited(space0, b'=', space0),
         entry_value,
-    )(i)
+    )
+    .parse_next(i)
 }
 
-const KEY_RE: &str = r"^[A-Za-z0-9-]+(\[[a-z]{2}(_[A-Z]{2})?(.[A-Za-z0-9-]+)?(@[A-Za-z09-]+)?\])?";
+fn entry_key(i: &mut &[u8]) -> Result<String> {
+    let identifier = || take_while(1.., (b'-', AsChar::is_alphanum));
 
-fn entry_key(i: &[u8]) -> IResult<'_, String> {
-    static RE_CELL: OnceLock<Regex> = OnceLock::new();
-    let key_re = RE_CELL.get_or_init(|| Regex::new(KEY_RE).unwrap());
+    let locale = (
+        take_while(2, b'a'..=b'z'),
+        opt(preceded(b'_', take_while(2, b'A'..=b'Z'))),
+        opt(preceded(b'.', identifier())),
+        opt(preceded(b'@', identifier())),
+    );
+    let bytes = (identifier(), opt(delimited(b'[', locale, b']')))
+        .take()
+        .parse_next(i)?;
 
-    re_find(key_re.clone())
-        .map(|name| {
-            // regex already garantees name is ascii
-            unsafe { str::from_utf8_unchecked(name) }.to_owned()
-        })
-        .parse(i)
+    // SAFETY: the parser should already guarantee the value is ascii
+    let key = unsafe { str::from_utf8_unchecked(bytes) }.to_owned();
+    Ok(key)
 }
 
-fn entry_value(i: &[u8]) -> IResult<'_, String> {
-    let (mut rest, line) = i.split_at_position_complete(|c| c == b'\n')?;
-    if !rest.is_empty() {
-        rest = &rest[1..];
-    }
-    match str::from_utf8(line) {
-        //
-        Ok(line) => Ok((rest, line.to_string())),
-        Err(_) => Err(nom::Err::Failure(ParseError::NonUtf8)),
-    }
+fn entry_value(i: &mut &[u8]) -> Result<String> {
+    // Technically, it should be until "\n", but till_line_ending also looks for
+    // "\r\n", and if that happens, the value should technically include the "\r", but
+    // in practice I don't think that is an issue, and is more likely an issue with
+    // the desktop file using the wrong line endings.
+    let line = till_line_ending.try_map(str::from_utf8).parse_next(i)?;
+    let _ = i.next_token();
+    Ok(line.to_string())
 }
 
 fn is_header_char(c: u8) -> bool {
@@ -128,17 +118,20 @@ mod test {
 
     #[test]
     fn entry_value_test_empty() {
-        assert_eq!(entry_value(&[][..]), Ok((&[][..], "".to_string())));
+        assert_eq!(
+            entry_value.parse_peek(&[][..]),
+            Ok((&[][..], "".to_string()))
+        );
     }
 
     #[test]
     fn entry_value_test_basic() {
         assert_eq!(
-            entry_value(&b"A simple value"[..]),
+            entry_value.parse_peek(&b"A simple value\n"[..]),
             Ok((&b""[..], "A simple value".to_string()))
         );
         assert_eq!(
-            entry_value(&b"A simple value\n"[..]),
+            entry_value.parse_peek(&b"A simple value"[..]),
             Ok((&b""[..], "A simple value".to_string()))
         );
     }
@@ -146,49 +139,49 @@ mod test {
     #[test]
     fn entry_value_test_escapes() {
         assert_eq!(
-            entry_value(&b"\\s\\n\\t\\r\\\\\\a"[..]),
+            entry_value.parse_peek(&b"\\s\\n\\t\\r\\\\\\a"[..]),
             Ok((&b""[..], "\\s\\n\\t\\r\\\\\\a".to_string()))
         );
         assert_eq!(
-            entry_value(&b"Content with trailing slash \\"[..]),
+            entry_value.parse_peek(&b"Content with trailing slash \\"[..]),
             Ok((&b""[..], "Content with trailing slash \\".to_string()))
         )
     }
 
     #[test]
     fn entry_value_test_invalid_utf8() {
-        assert!(entry_value(&[0xc0, 0xc1]).is_err());
-        assert!(entry_value(&[0x80, 0xc1]).is_err());
+        assert!(entry_value.parse_peek(&[0xc0, 0xc1]).is_err());
+        assert!(entry_value.parse_peek(&[0x80, 0xc1]).is_err());
     }
 
     #[test]
     fn entry_key_test_locales() {
         assert_eq!(
-            entry_key(&b"Name[en_US.UTF-8@shaw]"[..]),
+            entry_key.parse_peek(&b"Name[en_US.UTF-8@shaw]"[..]),
             Ok((&b""[..], "Name[en_US.UTF-8@shaw]".to_string()))
         );
         assert_eq!(
-            entry_key(&b"Name[en_US.UTF-8]"[..]),
+            entry_key.parse_peek(&b"Name[en_US.UTF-8]"[..]),
             Ok((&b""[..], "Name[en_US.UTF-8]".to_string()))
         );
         assert_eq!(
-            entry_key(&b"Name[en_US@shaw]"[..]),
+            entry_key.parse_peek(&b"Name[en_US@shaw]"[..]),
             Ok((&b""[..], "Name[en_US@shaw]".to_string()))
         );
         assert_eq!(
-            entry_key(&b"Name[en.UTF-8@shaw]"[..]),
+            entry_key.parse_peek(&b"Name[en.UTF-8@shaw]"[..]),
             Ok((&b""[..], "Name[en.UTF-8@shaw]".to_string()))
         );
         assert_eq!(
-            entry_key(&b"Name[en_US]"[..]),
+            entry_key.parse_peek(&b"Name[en_US]"[..]),
             Ok((&b""[..], "Name[en_US]".to_string()))
         );
         assert_eq!(
-            entry_key(&b"Name[en.UTF-8]"[..]),
+            entry_key.parse_peek(&b"Name[en.UTF-8]"[..]),
             Ok((&b""[..], "Name[en.UTF-8]".to_string()))
         );
         assert_eq!(
-            entry_key(&b"Name[en@shaw]"[..]),
+            entry_key.parse_peek(&b"Name[en@shaw]"[..]),
             Ok((&b""[..], "Name[en@shaw]".to_string()))
         );
     }
@@ -216,12 +209,12 @@ Value4=5.6"[..];
             },
         )]);
 
-        assert_eq!(desktop_entry(bytes), Ok((&b""[..], expected)));
+        assert_eq!(desktop_entry.parse_peek(bytes), Ok((&b""[..], expected)));
     }
 
     #[test]
     fn parse_test() {
-        let input = "\
+        let input = b"\
 [Desktop Entry]
 #A comment
 Exec=env A=a B=b sample-prog --foo --bar
